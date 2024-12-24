f7fa1db3fc08eb48f7a2f91c1451d6095ab28fbd
WaterBean <gkstnqls9@gmail.com>
2024-12-24 18:51:21 +0900

    [Chore] Create 1st Assignment Project

---
+++ MovieProject/MovieProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieProject
+//
+//  Created by 한수빈 on 12/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

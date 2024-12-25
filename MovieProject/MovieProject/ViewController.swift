cd1372634317e1d8209ccf8033df56894fb69edd
WaterBean <gkstnqls9@gmail.com>
2024-12-25 20:53:37 +0900

    Create 2 View

--- MovieProject/MovieProject/ViewController.swift
+++ MovieProject/MovieProject/ViewController.swift
@@ -17,3 +17,39 @@ class ViewController: UIViewController {
 
 }
 
+
+// MARK: - Storyboard Inspectable Properties
+
+extension UIView {
+    @IBInspectable var borderColor: UIColor {
+        get {
+            let color = self.layer.borderColor ?? UIColor.clear.cgColor
+            return UIColor(cgColor: color)
+        }
+        
+        set {
+            self.layer.borderColor = newValue.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+        
+}

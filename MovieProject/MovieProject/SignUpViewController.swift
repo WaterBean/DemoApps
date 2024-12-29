4b788ba7dd13da4d744274b17c5060281bbe3456
WaterBean <gkstnqls9@gmail.com>
2024-12-30 00:27:46 +0900

    [Refactor] Improve code structure and validation logic

--- MovieProject/MovieProject/SignUpViewController.swift
+++ MovieProject/MovieProject/SignUpViewController.swift
@@ -7,99 +7,95 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
-
-    @IBOutlet var emailOrTelephoneTextField: UITextField!
-    @IBOutlet var passwordTextField: UITextField!
-    @IBOutlet var nicknameTextField: UITextField!
-    @IBOutlet var locationTextField: UITextField!
-    @IBOutlet var referralcodeTextField: UITextField!
-    @IBOutlet var signUpButton: UIButton!
-    @IBOutlet var additionalInformationSwitch: UISwitch!
+final class SignUpViewController: UIViewController {
+    
+    @IBOutlet private var emailOrTelephoneTextField: UITextField!
+    @IBOutlet private var passwordTextField: UITextField!
+    @IBOutlet private var nicknameTextField: UITextField!
+    @IBOutlet private var locationTextField: UITextField!
+    @IBOutlet private var referralcodeTextField: UITextField!
+    @IBOutlet private var signUpButton: UIButton!
+    @IBOutlet private var additionalInformationSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        emailOrTelephoneTextField.attributedPlaceholder = NSAttributedString(string: "이메일 주소 또는 전화번호", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
-        emailOrTelephoneTextField.textColor = .white
-        emailOrTelephoneTextField.keyboardType = .emailAddress
-        emailOrTelephoneTextField.textAlignment = .center
-        emailOrTelephoneTextField.borderStyle = .roundedRect
-        emailOrTelephoneTextField.backgroundColor = .gray
-        emailOrTelephoneTextField.enablesReturnKeyAutomatically = true
+        configureTextField(emailOrTelephoneTextField, placeHolderText: "이메일 주소 또는 전화번호", keyboardType: .emailAddress)
+        configureTextField(passwordTextField, placeHolderText: "비밀번호", isSecureEntry: true)
+        configureTextField(nicknameTextField, placeHolderText: "닉네임")
+        configureTextField(locationTextField, placeHolderText: "위치", enablesReturnKeyAutomatically: false)
+        configureTextField(referralcodeTextField, placeHolderText: "추천 코드 입력", keyboardType: .numberPad, enablesReturnKeyAutomatically: false)
         
-        passwordTextField.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
-        passwordTextField.textColor = .white
-        passwordTextField.keyboardType = .default
-        passwordTextField.isSecureTextEntry = true
-        passwordTextField.textAlignment = .center
-        passwordTextField.borderStyle = .roundedRect
-        passwordTextField.backgroundColor = .gray
-        passwordTextField.enablesReturnKeyAutomatically = true
-
-        nicknameTextField.attributedPlaceholder = NSAttributedString(string: "닉네임", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
-        nicknameTextField.textColor = .white
-        nicknameTextField.keyboardType = .default
-        nicknameTextField.textAlignment = .center
-        nicknameTextField.borderStyle = .roundedRect
-        nicknameTextField.backgroundColor = .gray
-        nicknameTextField.enablesReturnKeyAutomatically = true
-
-        locationTextField.attributedPlaceholder = NSAttributedString(string: "위치", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
-        locationTextField.textColor = .white
-        locationTextField.keyboardType = .default
-        locationTextField.textAlignment = .center
-        locationTextField.borderStyle = .roundedRect
-        locationTextField.backgroundColor = .gray
-
-        referralcodeTextField.attributedPlaceholder = NSAttributedString(string: "추천 코드 입력", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
-        referralcodeTextField.textColor = .white
-        referralcodeTextField.keyboardType = .numberPad
-        referralcodeTextField.textAlignment = .center
-        referralcodeTextField.borderStyle = .roundedRect
-        referralcodeTextField.backgroundColor = .gray
-        
+        configureButton()
+        configureSwitch()
+    }
+    
+    private func configureTextField(_ textField: UITextField, textColor: UIColor = .white, backgroudColor: UIColor = .gray, borderStyle: UITextField.BorderStyle = .roundedRect, textAlignment: NSTextAlignment = .center, placeHolderText: String, placeHolderColor: UIColor = .white, keyboardType: UIKeyboardType = .default, isSecureEntry: Bool = false, enablesReturnKeyAutomatically: Bool = true) {
+        textField.textColor = textColor
+        textField.backgroundColor = backgroudColor
+        textField.borderStyle = borderStyle
+        textField.textAlignment = textAlignment
+        textField.attributedPlaceholder = NSAttributedString(string: placeHolderText, attributes: [NSAttributedString.Key.foregroundColor : placeHolderColor])
+        textField.keyboardType = keyboardType
+        textField.isSecureTextEntry = isSecureEntry
+        textField.enablesReturnKeyAutomatically = enablesReturnKeyAutomatically
+    }
+    
+    private func configureSwitch() {
+        additionalInformationSwitch.setOn(true, animated: true)
+        additionalInformationSwitch.onTintColor = .systemPink
+        additionalInformationSwitch.thumbTintColor = .white
+    }
+    
+    private func configureButton() {
         signUpButton.setTitle("회원가입", for: .normal)
         signUpButton.setTitleColor(.black, for: .normal)
         signUpButton.layer.backgroundColor = UIColor.white.cgColor
         signUpButton.clipsToBounds = true
         signUpButton.layer.cornerRadius = 6
         signUpButton.titleLabel?.font = .systemFont(ofSize: 14, weight: .bold)
-        
-        additionalInformationSwitch.setOn(true, animated: true)
-        additionalInformationSwitch.onTintColor = .systemPink
-        additionalInformationSwitch.thumbTintColor = .white
     }
-
     
-    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+    
+    private func validateEmailOrTelephone() -> Bool {
+        let isValid = emailOrTelephoneTextField.hasText ? true : false
+        emailOrTelephoneTextField.textColor = isValid ? .white : .red
+        return isValid
+    }
+    
+    private func validatePassword() -> Bool {
+        guard let password = passwordTextField.text else { return false }
+        let isValid = password.count >= 6 ? true : false
+        passwordTextField.textColor = isValid ? .white : .red
+        return isValid
+    }
+    
+    private func validateReferralCode() -> Bool {
+        guard let referralcode = referralcodeTextField.text else { return false }
+        let isValid = referralcode.allSatisfy { $0.isNumber } ? true : false
+        referralcodeTextField.textColor = isValid ? .white : .red
+        return isValid
+    }
+    
+    @IBAction private func signUpButtonTapped(_ sender: UIButton) {
         view.endEditing(true)
-        //Mission 1
-        if let _ = emailOrTelephoneTextField.text {
-            if !emailOrTelephoneTextField.hasText {
-                emailOrTelephoneTextField.textColor = .red
-            } else {
-                emailOrTelephoneTextField.textColor = .white
-            }
-        }
         
-        if let passwordText = passwordTextField.text {
-            if !passwordTextField.hasText || passwordText.count < 6 {
-                passwordTextField.textColor = .red
-            } else {
-                passwordTextField.textColor = .white
-                
-            }
-        }
+        let isValidEmailOrTelephone = validateEmailOrTelephone()
+        let isValidPassword = validatePassword()
+        let isValidReferralCode = validateReferralCode()
         
-        if let referralcodeText = referralcodeTextField.text {
-            if !referralcodeText.allSatisfy({ $0.isNumber }) {
-                referralcodeTextField.textColor = UIColor.red
-            } else {
-                referralcodeTextField.textColor = .white
-            }
+        if isValidEmailOrTelephone && isValidPassword && isValidReferralCode {
+            // TODO: - 회원가입 성공로직
+            print("회원가입 성공")
+        } else {
+            // TODO: - 회원가입 실패로직
+            print("회원가입 실패")
         }
     }
     
+    @IBAction private func hideKeyboardAction(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+        
+    }
     
 }

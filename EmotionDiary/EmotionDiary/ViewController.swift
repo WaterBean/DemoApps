a666c1ed343e73d5f4ea34da0024f8b52d197113
WaterBean <gkstnqls9@gmail.com>
2024-12-29 16:47:12 +0900

    [Refactor] Extract emotion label update methods

--- EmotionDiary/EmotionDiary/ViewController.swift
+++ EmotionDiary/EmotionDiary/ViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-// TODO: - Refactor IBOutlet to IBOutlet Collection
 final class ViewController: UIViewController {
     
     @IBOutlet private var emotionButtons: [UIButton]!
@@ -19,6 +18,14 @@ final class ViewController: UIViewController {
         3: ("당황해", 1), 4: ("속상해", 3), 5: ("우울해", 6),
         6: ("심심해", 0), 7: ("행복해", 1), 8: ("행복해", 0)]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.layer.backgroundColor = UIColor.init(red: 231, green: 211, blue: 221, alpha: 1).cgColor
+        configureBarButtonItem()
+        configureEmotionButtonsAndLabels()
+    }
+    
     private func configureBarButtonItem() {
         navBarButtonItem.setSymbolImage(UIImage(systemName: "list.bullet") ?? UIImage(), contentTransition: .automatic)
         navBarButtonItem.tintColor = .black
@@ -29,27 +36,28 @@ final class ViewController: UIViewController {
             emotionButtons[i].tag = i
             emotionLabels[i].tag = i
         }
-        emotionLabels.forEach {
-            $0.text = "\(emotionInfo[$0.tag]!.0)  \(String(emotionInfo[$0.tag]!.1))"
-        }
+        
+        updateEmotionLabels()
+        
         emotionButtons.forEach {
             $0.setImage(UIImage(named: "mono_slime\($0.tag+1)"), for: .normal)
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        view.layer.backgroundColor = UIColor.init(red: 231, green: 211, blue: 221, alpha: 1).cgColor
-                
-        configureBarButtonItem()
-        configureEmotionButtonsAndLabels()
+    private func updateEmotionLabels() {
+        emotionLabels.forEach { updateEmotionLabel(at: $0.tag)
+        }
+    }
+
+    private func updateEmotionLabel(at index: Int) {
+        guard let (name, count) = emotionInfo[index] else { return }
+        emotionLabels[index].text = "\(name)  \(count)"
     }
 
     @IBAction private func emotionButtonTapped(_ sender: UIButton) {
         emotionInfo[sender.tag]?.1 += 1
-        emotionLabels[sender.tag].text =
-        "\(emotionInfo[sender.tag]!.0)  \(String(emotionInfo[sender.tag]!.1))"
+        updateEmotionLabel(at: sender.tag)
     }
+    
 }
 

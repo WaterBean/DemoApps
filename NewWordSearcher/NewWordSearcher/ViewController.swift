f802894d73d4141c712c9cb5d40683e902f76d01
WaterBean <gkstnqls9@gmail.com>
2024-12-29 17:06:35 +0900

    [Chore] Create new project

---
+++ NewWordSearcher/NewWordSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewWordSearcher
+//
+//  Created by 한수빈 on 12/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

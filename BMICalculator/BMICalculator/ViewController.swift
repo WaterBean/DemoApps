fcabd69eb4b28c16946858a93aefc41947fbe592
WaterBean <gkstnqls9@gmail.com>
2024-12-30 18:38:52 +0900

    [Chore] Create BMICalculator project

---
+++ BMICalculator/BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by 한수빈 on 12/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

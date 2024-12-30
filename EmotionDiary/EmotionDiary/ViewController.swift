74121bee4c635ca4d47b539e7018263c988f1d9d
WaterBean <gkstnqls9@gmail.com>
2024-12-30 21:51:50 +0900

    [Feat] implement save count logic

--- EmotionDiary/EmotionDiary/ViewController.swift
+++ EmotionDiary/EmotionDiary/ViewController.swift
@@ -13,17 +13,26 @@ final class ViewController: UIViewController {
     @IBOutlet private var emotionLabels: [UILabel]!
     @IBOutlet private var navBarButtonItem: UIBarButtonItem!
     
-    private var emotionInfo: [Int: (String, Int)] = [
-        0: ("행복해", 2), 1: ("사랑해", 20), 2: ("좋아해", 3),
-        3: ("당황해", 1), 4: ("속상해", 3), 5: ("우울해", 6),
-        6: ("심심해", 0), 7: ("행복해", 1), 8: ("행복해", 0)]
+    private var emotionInfo: [Int: (name: String, count: Int)] = [
+        0: ("행복해", 0), 1: ("사랑해", 0), 2: ("좋아해", 0),
+        3: ("당황해", 0), 4: ("속상해", 0), 5: ("우울해", 0),
+        6: ("심심해", 0), 7: ("그냥해", 0), 8: ("계속해", 0)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.layer.backgroundColor = UIColor.init(red: 231, green: 211, blue: 221, alpha: 1).cgColor
         configureBarButtonItem()
+        loadEmotionInfo()
         configureEmotionButtonsAndLabels()
+    }
+    
+    private func loadEmotionInfo() {
+        emotionInfo.forEach { (key: Int, value: (name: String, count: Int)) in
+            let savedCount = UserDefaults.standard.integer(forKey: String(key))
+            print("\(emotionInfo[key]!) -> count: \(savedCount)")
+            emotionInfo[key] = (name: value.name, count: savedCount)
+        }
     }
     
     private func configureBarButtonItem() {
@@ -54,6 +63,7 @@ final class ViewController: UIViewController {
     private func updateEmotionLabel(at index: Int) {
         guard let (name, count) = emotionInfo[index] else { return }
         emotionLabels[index].text = "\(name)  \(count)"
+        UserDefaults.standard.set(count, forKey: String(index))
     }
 
     @IBAction private func emotionButtonTapped(_ sender: UIButton) {

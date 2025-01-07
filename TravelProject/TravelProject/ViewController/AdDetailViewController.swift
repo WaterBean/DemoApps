7dba8c2ee7813f1929763861a18383ffe3ffdc2f
WaterBean <gkstnqls9@gmail.com>
2025-01-07 14:33:54 +0900

    [Chore] 파일 생성 및 씬, 로직 파일 연결

---
+++ TravelProject/TravelProject/ViewController/AdDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AdDetailViewController.swift
+//  TravelProject
+//
+//  Created by 한수빈 on 1/7/25.
+//
+
+import UIKit
+
+final class AdDetailViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
---
+++ TravelProject/TravelProject/ViewController/TravelInfoDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TravelInfoDetailViewController.swift
+//  TravelProject
+//
+//  Created by 한수빈 on 1/7/25.
+//
+
+import UIKit
+
+final class TravelInfoDetailViewController: UIViewController {
+
+    @IBOutlet var detailImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleView: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

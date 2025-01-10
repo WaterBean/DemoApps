eaf9672b5921065cd26629362ad0d6cf9ff41b88
WaterBean <gkstnqls9@gmail.com>
2025-01-10 22:27:58 +0900

    [Chore] 뷰,셀 파일 생성 및 뷰 배치

---
+++ TravelProject/TravelProject/Cell/ChatWrittenByMeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatWrittenByMeTableViewCell.swift
+//  TravelProject
+//
+//  Created by 한수빈 on 1/10/25.
+//
+
+import UIKit
+
+class ChatWrittenByMeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

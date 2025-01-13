5af0a1cb4b34d047f23df0f95c4b99ea61aba526
WaterBean <gkstnqls9@gmail.com>
2025-01-13 15:53:13 +0900

    [Chore] snapkit 패키지 의존성 추가

---
+++ MovieProject/MovieProject/SignUpCodeBaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpCodeBaseViewController.swift
+//  MovieProject
+//
+//  Created by 한수빈 on 1/13/25.
+//
+
+import UIKit
+
+final class SignUpCodeBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
